6e8400efb033d58edbf33262067aab758e7e4e34
divic-codes <itsrusheryt@gmail.com>
2026-03-17 15:49:12 +0530

    Add files via upload

---
+++ count.c
@@ -0,0 +1,15 @@
+// To count how many numbers from 1-100 are divisible by 9.
+#include<stdio.h>
+int main(){
+    int i;
+    int count=0;
+    for(i=1;i<=100;i++){
+        if(i%9==0){
+            count++;
+        }
+}
+printf("%d",count);
+}
+    
+
+
