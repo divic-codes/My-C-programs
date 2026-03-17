716f0c946cf436a37fb59e9a5980f9224a57bdc3
divic-codes <itsrusheryt@gmail.com>
2026-03-17 13:30:14 +0530

    Update evenodd.c

--- evenodd.c
+++ evenodd.c
@@ -1,1 +1,13 @@
-
+// TO CHECK IF A NUMBER IS EVEN OR ODD.
+#include<stdio.h>
+int main(){
+  int num;
+printf("Enter a number: ");
+scanf("%d",&num);
+if(num%2==0){
+printf("%d is Even",num);
+}
+else{
+printf("%d is Odd",num);
+}
+}
