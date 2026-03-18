e9d51a45b45b7b8d55897062000dd17d8977273a
Divyansh Chhabra <itsrusheryt@gmail.com>
2026-03-18 13:49:45 +0530

    TO PRINT ALPHABETS FROM A TO Z.
    ASCII TABLE (STANDARD AMERICAN CODE FOR INFO & INTERCHANGE).

---
+++ ASCII.c
@@ -0,0 +1,14 @@
+// TO PRINT ALPHABETS FROM A TO Z.
+// ASCII TABLE (STANDARD AMERICAN CODE FOR INFO & INTERCHANGE).
+#include<stdio.h>
+int main(){
+    int i = 65;
+    while(i<=90){
+        printf(" %c ",i);
+        i++;
+    }
+    for(int i=97;i<=122;i++){
+        printf(" %c ",i);
+    } 
+
+}
