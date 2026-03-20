091f318a3eff786ff07f93d0ae11e9f76ee353f2
Divyansh Chhabra <itsrusheryt@gmail.com>
2026-03-20 21:39:06 +0530

    To Print numbers in a pattern.
    Using nested for loops to print numbers in a systematic pattern.

---
+++ num-pattern.c
@@ -0,0 +1,17 @@
+// TO PRINT NUMBERS 1 TO 5 IN A SYSTEMATIC PATTERN
+// LIKE GIVEN BELOW :
+// 1
+// 12
+// 123
+// 1234
+// 12345
+#include<stdio.h>
+int main(){
+    int i;
+    for(i=1; i < 6; i++){
+        for(int j = 1 ; j<=i;j++){
+            printf("%d",j);
+        }
+        
+}
+}
